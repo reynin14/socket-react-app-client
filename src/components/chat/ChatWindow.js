f57665f316e9d8faa99abac53d085b184ae0feb9
Kevin Yonzon <kyonzon14@gmail.com>
2018-02-25 19:16:58 -0800

    connecting props to chatWindow

--- src/components/chat/ChatWindow.js
+++ src/components/chat/ChatWindow.js
@@ -5,10 +5,11 @@ class ChatWindow extends React.Component{
   render(){
     return(
       <div className='chatWindow'>
+        {this.props.chatbox}
         <div>
           <textarea className='chatInput'></textarea>
           <div className='chatSubmit'>
-            <button>Enter</button>
+            <button onClick={this.props.handleUpdate}>Enter</button>
           </div>
         </div>
       </div>
